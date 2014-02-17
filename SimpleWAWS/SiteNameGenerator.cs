913083f2371fe23ffbd9d92759f81b9a3691407c
davidebbo <david.ebbo@microsoft.com>
2014-02-17 00:40:41 -0800

    Tweak word list

--- SimpleWAWS/SiteNameGenerator.cs
+++ SimpleWAWS/SiteNameGenerator.cs
@@ -5,9 +5,9 @@ namespace SimpleWAWS
     class SiteNameGenerator
     {
         private string[] _adjectives = new string[] { "Blue", "Green", "Orange", "Pink", "Purple", "Red", "Yellow", "Adventurous", "Bold", "Brave", "Bright", "Charming", "Cool", "Courageous",
-            "Creative", "Dazzling", "Dancing", "Eager", "Fearless", "Friendly", "Heroic", "Important", "Impressive", "Ingenious", "Insightful", "Intuitive", "Masterful",
-            "Mighty", "Modern", "Outstanding", "Smooth", "Patient", "Peaceful", "Popular", "Precise", "Productive", "Proficient", "Proud", "Resilient", "Resourceful", "Savvy", "Sociable",
-            "Sharp", "Shiny", "Skillful", "Spirited", "Strong", "Stunning", "Successful", "Talented", "Technical", "Tenacious", "Upbeat", "Valuable", "Victorious",
+            "Creative", "Dazzling", "Dancing", "Eager", "Fast", "Fearless", "Friendly", "Heroic", "Important", "Impressive", "Ingenious", "Insightful", "Intuitive", "Laughing", "Masterful",
+            "Mighty", "Modern", "Outstanding", "Smooth", "Patient", "Peaceful", "Popular", "Precise", "Productive", "Proud", "Resilient", "Resourceful", "Running", "Savvy", "Sociable",
+            "Sharp", "Shiny", "Skillful", "Speedy", "Spirited", "Strong", "Stunning", "Successful", "Talented", "Technical", "Tenacious", "Upbeat", "Valuable", "Victorious",
             "Vigorous", "Visionary", "Vital", "Vivacious", "Winning", "Wise", "Youthful", "Zealous", "Zestful"
         };
         private string[] _nouns = new string[] { "Albatross", "Alligator", "Ant", "Armadillo", "Badger", "Bear", "Bee", "Bird", "Bison", "Buffalo", "Caribou", "Cheetah", "Cobra",
@@ -15,7 +15,8 @@ namespace SimpleWAWS
             "Hamster", "Hawk", "Heron", "Hippo", "Horse", "Kangaroo", "Koala", "Kudu", "Lemur", "Leopard", "Lion", "Llama", "Lobster", "Manatee", "Meerkat", "Mink", "Mouse",
             "Narwhal", "Octopus", "Okapi", "Oryx", "Ostrich", "Otter", "Oyster", "Panther", "Parrot", "Pelican", "Penguin", "Pony", "Quail", "Rabbit", "Raven", "Rhino", "Robin",
             "Salmon", "Seahorse", "Seal", "Sparrow", "Spider", "Squid", "Squirrel", "Starling", "Stork", "Swan", "Tiger", "Trout", "Turkey", "Turtle", "Unicorn", "Walrus", "Wolf",
-            "Cloud", "Galaxy", "Moon", "Mountain", "Prairie", "Star", "Valley", "Wind", "Rocket" };
+            "Cloud", "Galaxy", "Moon", "Mountain", "Prairie", "Star", "Valley", "Wind", "Rocket"
+        };
         private Random _random = new Random();
 
         public string GenerateName(bool includeNumber)
