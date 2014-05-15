5726b66421cf759a928f3354351b842099d36278
Ahmed ElSayed <ahmels@microsoft.com>
2014-05-14 17:52:20 -0700

    returning http correct error codes to the client in case of a server error

--- SimpleWAWS/Controllers/SiteController.cs
+++ SimpleWAWS/Controllers/SiteController.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Runtime.InteropServices;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Hosting;
@@ -18,18 +20,19 @@ namespace SimpleWAWS.Controllers
     {
         [Route("{siteId}")]
         [HttpGet]
-        public async Task<Site> Get(string siteId)
+        public async Task<HttpResponseMessage> Get(string siteId)
         {
             var siteManager = await SiteManager.GetInstanceAsync();
-            return siteManager.GetSite(siteId);
+            return Request.CreateResponse(HttpStatusCode.OK, siteManager.GetSite(siteId));
         }
 
         [Route("~/api/reset")]
         [HttpGet]
-        public async Task Reset()
+        public async Task<HttpResponseMessage> Reset()
         {
             var siteManager = await SiteManager.GetInstanceAsync();
             await siteManager.ResetAllFreeSites();
+            return Request.CreateResponse(HttpStatusCode.Accepted);
         }
 
         [Route("getpublishingprofile/{siteId}")]
@@ -46,23 +49,32 @@ namespace SimpleWAWS.Controllers
         }
 
         [HttpPost]
-        public async Task<Site> Post([FromBody] Template template)
+        public async Task<HttpResponseMessage> Post([FromBody] Template template)
         {
-            var siteManager = await SiteManager.GetInstanceAsync();
-            return
-                await
-                    siteManager.ActivateSiteAsync(template == null
-                        ? null
-                        : TemplatesManager.GetTemplates()
-                            .SingleOrDefault(t => t.Name == template.Name && t.Language == template.Language));
+            try
+            {
+                var siteManager = await SiteManager.GetInstanceAsync();
+                var site = 
+                    await
+                        siteManager.ActivateSiteAsync(template == null
+                            ? null
+                            : TemplatesManager.GetTemplates()
+                                .SingleOrDefault(t => t.Name == template.Name && t.Language == template.Language));
+                return Request.CreateResponse(HttpStatusCode.OK, site);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.ServiceUnavailable, ex.Message);
+            }
         }
 
         [Route("{siteId}")]
         [HttpDelete]
-        public async Task Delete(string siteId)
+        public async Task<HttpResponseMessage> Delete(string siteId)
         {
             var siteManager = await SiteManager.GetInstanceAsync();
             await siteManager.DeleteSite(siteId);
+            return Request.CreateResponse(HttpStatusCode.Accepted);
         }
     }
 }
