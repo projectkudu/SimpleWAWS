74a92f791cf049291d06c00c1a6c1e280cd81cf5
Ahmed ElSayed <ahmels@microsoft.com>
2015-07-16 10:29:13 -0700

    Adding Client code for analytics

---
+++ Analytics/Client/src/js/routes.js
@@ -0,0 +1,43 @@
+'use strict';
+
+/**
+ * Route configuration for the RDash module.
+ */
+angular.module('RDash').config(['$stateProvider', '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider) {
+
+        // For unmatched routes
+        $urlRouterProvider.otherwise('/');
+
+        // Application routes
+        $stateProvider
+            .state('kpis', {
+                url: '/',
+                templateUrl: 'templates/graph.html'
+            })
+            .state('appCreates', {
+                url: '/appCreates',
+                templateUrl: 'templates/graph.html'
+            })
+            .state('accountTypes', {
+                url: '/accountTypes',
+                templateUrl: 'templates/graph.html'
+            })
+            .state('referrers', {
+                url: '/referrers',
+                templateUrl: 'templates/graph.html'
+            })
+            .state('templates', {
+                url: '/templates',
+                templateUrl: 'templates/graph.html'
+            })
+            .state('experiments', {
+                url: '/experiments',
+                templateUrl: 'templates/experiments.html'
+            })
+            .state('sourceVariations', {
+                url: '/sourceVariations',
+                templateUrl: 'templates/sourceVariations.html'
+            });
+    }
+]);
