ccf57188b99ce7e1e914334e23ecf0aee5aca32b
Ahmed ElSayed <ahmels@microsoft.com>
2015-07-16 09:12:48 -0700

    Adding TryAppService.WebJob.ProcessQueue Project

---
+++ TryAppService.WebJob.ProcessQueue/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Azure.WebJobs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryAppService.WebJob.ProcessQueue
+{
+    class Program
+    {
+        static void Main()
+        {
+            var host = new JobHost();
+            host.RunAndBlock();
+        }
+    }
+}
