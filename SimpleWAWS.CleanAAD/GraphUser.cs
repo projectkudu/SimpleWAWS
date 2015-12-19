04956847df008165ca1b406ddc81b239a72fc01b
Ahmed ElSayed <ahmels@microsoft.com>
2015-12-18 16:35:42 -0800

    Adding CleanAAD project

---
+++ SimpleWAWS.CleanAAD/GraphUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleWAWS.CleanAAD
+{
+    public class GraphArray
+    {
+        public GraphUser[] value { get; set; }
+    }
+    public class GraphUser
+    {
+        public string displayName { get; set; }
+        public string objectId { get; set; }
+        public DateTime? acceptedOn { get; set; }
+    }
+}
---
+++ SimpleWAWS.CleanAAD/Program.cs
@@ -0,0 +1,47 @@
+﻿using ARMClient.Library;
+using Newtonsoft.Json.Linq;
+using SimpleWAWS.Code;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleWAWS.CleanAAD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Run().Wait();
+        }
+
+        static async Task Run()
+        {
+            dynamic GraphClient;
+            GraphClient = await ARMLib.GetDynamicClient(apiVersion: "1.42-previewInternal", url: string.Format("{0}/{1}", "https://graph.windows.net", SimpleSettings.TryTenantId))
+                                .ConfigureLogin(LoginType.Upn, SimpleSettings.TryUserName, SimpleSettings.TryPassword);
+            var tasks = new List<Task>();
+            while (true)
+            {
+                var users = (GraphArray)await GraphClient.Users.Query("$top=999").GetAsync<GraphArray>();
+                foreach (var user in users.value)
+                {
+                    if (user.acceptedOn != null &&
+                        user.acceptedOn < DateTime.UtcNow.AddDays(-2) &&
+                        new[] { "ahmels", "graphAdmin", "trywebsitesnow" }.All(n => user.displayName.IndexOf(n, StringComparison.OrdinalIgnoreCase) == -1))
+                    {
+                        Console.WriteLine(user.displayName);
+                        tasks.Add(GraphClient.Users[user.objectId].DeleteAsync());
+                    }
+
+                    if (tasks.Count >= 50)
+                    {
+                        await Task.WhenAll(tasks);
+                        tasks.Clear();
+                    }
+                }
+            }
+        }
+    }
+}
