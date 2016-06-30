0f01a9fb6767a1774829798f8bcb04b2cb00f796
Faiz Shaikh <fashaikh@microsoft.com>
2016-06-30 16:46:30 -0700

    add redirect to function site

--- SimpleWAWS/Authentication/GoogleAuthProvider.cs
+++ SimpleWAWS/Authentication/GoogleAuthProvider.cs
@@ -18,14 +18,7 @@ namespace SimpleWAWS.Authentication
             builder.Append("https://accounts.google.com/o/oauth2/auth");
             builder.Append("?response_type=id_token");
             //TODO:Add a whitelisted location list
-            if (context.IsFunctionsPortalRequest())
-            {
-                builder.AppendFormat("&redirect_uri={0}", WebUtility.UrlEncode(string.Format(CultureInfo.InvariantCulture, "{0}/?Login=true", context.Request.Headers["Origin"])));
-            }
-            else
-            {
-                builder.AppendFormat("&redirect_uri={0}", WebUtility.UrlEncode(string.Format(CultureInfo.InvariantCulture, "https://{0}/Login", context.Request.Headers["HOST"])));
-            }
+            builder.AppendFormat("&redirect_uri={0}", WebUtility.UrlEncode(string.Format(CultureInfo.InvariantCulture, "https://{0}/Login", context.Request.Headers["HOST"])));
             builder.AppendFormat("&client_id={0}", AuthSettings.GoogleAppId);
             builder.AppendFormat("&scope={0}", "email");
             builder.AppendFormat("&state={0}", WebUtility.UrlEncode(context.IsAjaxRequest()|| context.IsFunctionsPortalRequest() ? string.Format(CultureInfo.InvariantCulture, "/{0}{1}", culture, context.Request.Url.Query) : context.Request.Url.PathAndQuery));
