d62dc156bb9b4177ff017ebecd85184a0f97cc9c
Faiz Shaikh <fashaikh@microsoft.com>
2016-07-01 15:36:53 -0700

    Move the redirect logic to the authenticate event

--- SimpleWAWS/Global.asax.cs
+++ SimpleWAWS/Global.asax.cs
@@ -134,18 +134,6 @@ namespace SimpleWAWS
             ExperimentManager.AssignExperiment(context);
             GlobalizationManager.SetCurrentCulture(context);
 
-            if (context.Request["state"]!=null)
-            if (context.Request["state"].Contains("appServiceName=Function"))
-            {
-                if (context.User != null)
-                {
-                    string cookie = CreateSessionCookieData(context.User);
-                    var a = context.Request["state"];
-                    var redirectlocation = a.Split('?')[0];
-                   Response.Redirect(String.Format("{0}?cookie={1}{2}",redirectlocation,cookie,context.Request.QueryString) ,true);
-                }
-            }
-
             //if (context.Request.Cookies[Constants.TiPCookie] == null &&
             //    context.Request.QueryString[Constants.TiPCookie] != null)
             //{
@@ -187,8 +175,22 @@ namespace SimpleWAWS
 
             if (!SecurityManager.TryAuthenticateSessionCookie(context))
             {
+                if (context.IsBrowserRequest())
+                {
+                    if (context.Request["state"] != null)
+                        if (context.Request["state"].Contains("appServiceName=Function"))
+                        {
+                            if (context.User != null)
+                            {
+                                string cookie = CreateSessionCookieData(context.User);
+                                var a = context.Request["state"];
+                                var redirectlocation = a.Split('?')[0];
+                                Response.Redirect(String.Format("{0}?cookie={1}{2}", redirectlocation, cookie, context.Request.QueryString), true);
+                            }
+                        }
+                }
                 // Support requests from non-browsers with bearer headers
-                if (!context.IsBrowserRequest() &&
+                    if (!context.IsBrowserRequest() &&
                     SecurityManager.TryAuthenticateBearer(context))
                 {
                     return;
