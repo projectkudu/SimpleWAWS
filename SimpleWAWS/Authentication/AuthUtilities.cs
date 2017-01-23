7d289e4d977a327ff69fd64cab4b0588177ff418
Faiz Shaikh <fashaikh@microsoft.com>
2017-01-23 13:49:05 -0800

    Add GitHub auth. Hide option in UI

--- SimpleWAWS/Authentication/AuthUtilities.cs
+++ SimpleWAWS/Authentication/AuthUtilities.cs
@@ -6,9 +6,19 @@ namespace SimpleWAWS.Authentication
 {
     public static class AuthUtilities
     {
-        public static string GetContentFromUrl(string url)
+        public static string GetContentFromUrl(string url, string method="GET", bool jsonAccept = false, bool addGitHubHeaders = false, string AuthorizationHeader ="")
         {
             var request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = method;
+            request.UserAgent = "Anything";
+            if (jsonAccept)
+                request.Accept = "application/json";
+            if (addGitHubHeaders)
+                request.Accept = "application/vnd.github.v3+json";
+            if (!String.IsNullOrEmpty(AuthorizationHeader))
+            {
+                request.Headers.Add("Authorization", AuthorizationHeader);
+            }
             using (var response = request.GetResponse())
             {
                 using (var reader = new StreamReader(response.GetResponseStream()))
