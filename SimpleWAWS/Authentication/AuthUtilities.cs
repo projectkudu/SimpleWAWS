a798b5e2e16f5b33468fec6754c5c594a8173ec9
Faiz Shaikh <fashaikh@microsoft.com>
2017-01-25 11:48:06 -0800

    CR changes: refactor GetContentFromUrl handle null for primaryEmail from GitHub and identity in BaseAuthprovider

--- SimpleWAWS/Authentication/AuthUtilities.cs
+++ SimpleWAWS/Authentication/AuthUtilities.cs
@@ -6,15 +6,30 @@ namespace SimpleWAWS.Authentication
 {
     public static class AuthUtilities
     {
-        public static string GetContentFromUrl(string url, string method="GET", bool jsonAccept = false, bool addGitHubHeaders = false, string AuthorizationHeader ="")
+        public static string GetContentFromUrl(string url)
+        {
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            using (var response = request.GetResponse())
+            {
+                using (var reader = new StreamReader(response.GetResponseStream()))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+        }
+        public static string GetContentFromGitHubUrl(string url, string method = "GET", bool jsonAccept = false, bool addGitHubHeaders = false, string AuthorizationHeader = "")
         {
             var request = (HttpWebRequest)WebRequest.Create(url);
             request.Method = method;
-            request.UserAgent = "Anything";
+            request.UserAgent = "x-ms-try-appservice";
             if (jsonAccept)
+            {
                 request.Accept = "application/json";
+            }
             if (addGitHubHeaders)
+            {
                 request.Accept = "application/vnd.github.v3+json";
+            }
             if (!String.IsNullOrEmpty(AuthorizationHeader))
             {
                 request.Headers.Add("Authorization", AuthorizationHeader);
