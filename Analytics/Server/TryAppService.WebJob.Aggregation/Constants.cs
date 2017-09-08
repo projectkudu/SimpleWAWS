82403555425e055a59a2af62880bcde55b22dcbd
Faiz Shaikh <fashaikh@microsoft.com>
2017-09-08 13:00:09 -0700

    Remove Analytics Projects

--- Analytics/Server/TryAppService.WebJob.Aggregation/Constants.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace TryAppService.WebJob.Aggregation
-{
-    class Constants
-    {
-        public static readonly DateTime CreationMoment = new DateTime(2014, 7, 31, 16, 0, 0, DateTimeKind.Utc);
-    }
-}
